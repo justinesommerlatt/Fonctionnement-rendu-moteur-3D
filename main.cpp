4c7ed2181050bcf6c9a37883e34ad9d6b8b99b78
Justine <justinesommerlatt@gmail.com>
2022-02-04 09:47:45 +0100

    Correction z-buffer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "matrix.h"
 #include "geometry.h"
 #include "model.h"
+#include <array>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -52,12 +53,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage &img)
 }
 
 /*
-* Le barycentre d'un triangle est en fait son centre de gravité
-* Pour calculer le barycentre d'un triangle il existe plusieurs méthodes : 
-* - intersection des médianes
+* Le barycentre d'un triangle est en fait son centre de gravite
+* Pour calculer le barycentre d'un triangle il existe plusieurs methodes : 
+* - intersection des medianes
 * - ratio 2:1
-* - moyenne des coordonnées
-* Dans ce cas présent, calculer la moyenne des coordonnées se révèle être bien plus simple  
+* - moyenne des coordonnees
+* Dans ce cas present, calculer la moyenne des coordonnÃ©es se revele etre bien plus simple  
 */
 Vec2i barycentre(Vec2i s1, Vec2i s2, Vec2i s3){
     float x_center = (s1.x + s2.x + s3.x)/3;
@@ -67,49 +68,45 @@ Vec2i barycentre(Vec2i s1, Vec2i s2, Vec2i s3){
 
 }
 
+double triangle_area_2d(Vec2f a, Vec2f b, Vec2f c) {
+    return .5*((b.y-a.y)*(b.x+a.x) + (c.y-b.y)*(c.x+b.x) + (a.y-c.y)*(a.x+c.x));
+}
+
 /*
-* Calculer les coordonnées barycentriques 
+* Calculer les coordonnees barycentriques 
 * Pour cela, il nous faut calculer 4 aires de triangles (le grand triangles ainsi que les trois sous triangles qui le forment)
-* Puisque la somme des coordonées vaut 1, il nous suffit de calculer deux coordonnées pour connaitre la troisième, soit : 
+* Puisque la somme des coordonees vaut 1, il nous suffit de calculer deux coordonnees pour connaitre la troisieme, soit : 
 * c' = 1 - a' - b'
 * Aire d'un triangle = H*b /2
-* Aire d'un triangles à l'aide des coordonnées = 1/2 (s1.x(s2.Y - s3.y) + s2.x(s3.y - s1;Y) + s3.x(s1.y - s2.y))
+* Aire d'un triangles Ã  l'aide des coordonnÃ©es = 1/2 (s1.x(s2.Y - s3.y) + s2.x(s3.y - s1;Y) + s3.x(s1.y - s2.y))
 * s1 : sommet 1
 * s2 : sommet 2
 * s3 : sommet 3
-* bp : point dont on calcule les coordonnées barycentriques
+* bp : point dont on calcule les coordonnÃ©es barycentriques
 * s1p : s1' 
 * s2p : s2'
 * s3p : s3'
 */
-Vec3f barycentric(Vec3f s1, Vec3f s2, Vec3f s3, Vec3f bp){
-
-    double total_height = s3.y - s1.y;
-    double total_area = 1/2 * (s3.x * (s2.y - s1.y) + s2.x * (s1.y - s3.y) + s1.x * (s3.y - s2.y));
-    double area_triangle1 = 1/2 * (s3.x * (s2.y - bp.y) + s2.x * (bp.y - s3.y) + bp.x * (s3.y - s2.y));
-    double area_triangle2 = 1/2 * (s3.x * (bp.y - s1.y) + bp.x * (s1.y - s3.y) + s1.x * (s3.y - bp.y));
-    double area_triangle3 = 1/2 * (bp.x * (s2.y - s1.y) + s2.x * (s1.y - bp.y) + s1.x * (bp.y - s2.y));
-    double s1p = area_triangle1 / total_area;
-    double s2p = area_triangle2 / total_area;
-    double s3p = area_triangle3 / total_area;
-    // je choisis de calculer les trois aires afin de vérifier mes calculs
-    if (s1p + s2p + s3p == 1){
-        return Vec3f(s1p, s2p, s3p);
-    }
-    
+Vec3f barycentric(Vec2f s1, Vec2f s2, Vec2f s3, Vec2f bp){
+    double total_area = triangle_area_2d(s1, s2, s3);
+    if (total_area<0) return {-1,0,0}; // backface culling
+    double a = triangle_area_2d(bp, s2, s3);
+    double b = triangle_area_2d(bp, s3, s1);
+    double c = triangle_area_2d(bp, s1, s2);
+    return {static_cast<float>(a/total_area),static_cast<float>(b/total_area),static_cast<float>(c/total_area)};
 }
 
 /*
 * void triangle
-* entrées
+* entrÃ©es
 * s1 : premier sommet du triangle
-* s2 : deuxième sommet du triangle
-* s3 : troisième sommet du triangle
+* s2 : deuxiÃ¨me sommet du triangle
+* s3 : troisiÃ¨me sommet du triangle
 * &image : 
 * color : couleur du triangle
 *
 void triangle(Vec2i s1, Vec2i s2, Vec2i s3, TGAImage &image, TGAColor color) {
-    //si les trois coordonnées y sont égales alors il ne s'agit plus d'un triangle mais d'un segment
+    //si les trois coordonnÃ©es y sont Ã©gales alors il ne s'agit plus d'un triangle mais d'un segment
     if (s1.y==s2.y && s1.y==s3.y) return; 
     if (s1.y>s2.y) std::swap(s1, s2);
     if (s1.y>s3.y) std::swap(s1, s3);
@@ -121,22 +118,22 @@ void triangle(Vec2i s1, Vec2i s2, Vec2i s3, TGAImage &image, TGAColor color) {
 
 
     for (int i=0; i<total_height; i++) {
-        // on coupe le triangle en deux sous triangles et on regarde si on est dans la deuxième moitié (=triangle supérieur)
+        // on coupe le triangle en deux sous triangles et on regarde si on est dans la deuxiÃ¨me moitiÃ© (=triangle supÃ©rieur)
         bool second_half = i>s2.y-s1.y || s2.y==s1.y;
 
         // puisque le sommet s3 est le plus haut et le sommet s1 le plus bas, 
-        // la hauteur de notre sous triangle vaut soit s3.y-s2.y si on se trouve dans le triangle supérieur
-        // soit s2.y-s1.y si on se trouve dans le triangle inférieur
+        // la hauteur de notre sous triangle vaut soit s3.y-s2.y si on se trouve dans le triangle supÃ©rieur
+        // soit s2.y-s1.y si on se trouve dans le triangle infÃ©rieur
         int segment_height = second_half ? s3.y-s2.y : s2.y-s1.y;
 
         // hauteur relative de i dans le triangle courant
         float alpha = (float)i/total_height;
 
-        // si on se trouve dans le triangle supérieur alors on "supprime" le triangle inférieur (si on se trouve dans le triangle inférieur alors on n'enlève rien),
+        // si on se trouve dans le triangle supÃ©rieur alors on "supprime" le triangle infÃ©rieur (si on se trouve dans le triangle infÃ©rieur alors on n'enlÃ¨ve rien),
         // afin d'obtenir la hauteur de i relative dans le triangle courant
         float beta  = (float)(i-(second_half ? s2.y-s1.y : 0))/segment_height; 
 
-        // s3-s1 : vecteur allant de s3 à s1
+        // s3-s1 : vecteur allant de s3 Ã  s1
         // alpha : hauteur relative de i 
         Vec2i A =               s1 + (s3-s1)*alpha;
         Vec2i B = second_half ? s2 + (s3-s2)*beta : s1 + (s2-s1)*beta;
@@ -155,7 +152,7 @@ void triangle(Vec2i s1, Vec2i s2, Vec2i s3, TGAImage &image, TGAColor color) {
         }
     }
 }*/
-void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+void triangle(std::array<Vec3f,3> pts, float *zbuffer, TGAImage &image, TGAColor color) {
     Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
     Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
     Vec2f clamp(image.get_width()-1, image.get_height()-1);
@@ -165,17 +162,14 @@ void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
             bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
         }
     }
-    Vec3f P;
-    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
-        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
-            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+    for (double x=bboxmin.x; x<=bboxmax.x; x+=1) {
+        for (double y=bboxmin.y; y<=bboxmax.y; y+=1) {
+            Vec3f bc_screen  = barycentric(Vec2f{pts[0].x, pts[0].y}, Vec2f{pts[1].x, pts[1].y}, Vec2f{pts[2].x, pts[2].y}, Vec2f{static_cast<float>(x), static_cast<float>(y)});
             if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
-            P.z = 0;
-            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
-            if (zbuffer[int(P.x+P.y*width)]<P.z) {
-                zbuffer[int(P.x+P.y*width)] = P.z;
-                image.set(P.x, P.y, color);
-            }
+            double z = pts[0].z * bc_screen[0] + pts[1].z * bc_screen[1] + pts[2].z * bc_screen[2];
+            if (zbuffer[int(x+y*width)]>z) continue;
+            zbuffer[int(x+y*width)] = z;
+            image.set(x, y, color);
         }
     }
 }
@@ -186,7 +180,7 @@ Vec3f world2screen(Vec3f v) {
 }
 
 /* compareZ
-* je souhaite calculer la "moyenne" des coordonnées z afin d'ensuite pouvoir les trier
+* je souhaite calculer la "moyenne" des coordonnÃ©es z afin d'ensuite pouvoir les trier
 */ 
 float compareZ(Vec3f vect[]){
     return (vect[0].z + vect[1].z + vect[2].z)/3;
@@ -209,12 +203,12 @@ int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
 
     // un vecteur qui va contenir tous mes triangles
-    std::vector<Vec3f[3]> vec_triangles ;
+    std::vector<std::array<Vec3f, 3>> vec_triangles ;
 
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         
-        Vec3f pts[3];
+        std::array<Vec3f,3> pts;
         for (int i=0; i<3; i++) pts[i] = world2screen(model->vert(face[i]));
 
         //je mets dans mon vecteur tous les triangles
@@ -222,13 +216,13 @@ int main(int argc, char** argv) {
         //triangle(pts, zbuffer, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
 
-    // je trie mes triangles dans mon vecteur en fonction de la moyenne de leurs z afin de les traiter du plus profond au plus avancé et "superposer"
-    std::sort(vec_triangles.begin(), vec_triangles.end(), compareV);
+    // je trie mes triangles dans mon vecteur en fonction de la moyenne de leurs z afin de les traiter du plus profond au plus avancÃ© et "superposer"
+//  std::sort(vec_triangles.begin(), vec_triangles.end(), compareV);
     for (int i=0; i<vec_triangles.size(); i++){
          triangle(vec_triangles[i], zbuffer, image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
     }
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+//  image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
     return 0;
